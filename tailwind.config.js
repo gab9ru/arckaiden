4734b538c806c0bdba4b3e5e86b436d4d48feee3
gab9ru <gabirusama72769@gmail.com>
2025-09-02 11:02:48 +0800

    1.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./dist/**/*.{html,js}",    // scan all HTML/JS in dist
+    "./src/**/*.{js,ts,jsx,tsx}" // scan JS/TS in src
+  ],
   theme: {
     extend: {},
   },
